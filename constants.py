eb74287411d2998e70a4a27c1c5cdc91028bd91a
Ruslan Novikov <dib.08@mail.ru>
2024-03-17 00:52:06 +0300

    added functions draw_grid,create_grid

--- constants.py
+++ constants.py
@@ -2,3 +2,4 @@ width_screen = 350
 height_screen = 370
 min_width_screen = 250
 min_height_screen = 150
+cell_size = 20
--- main.py
+++ main.py
@@ -1,11 +1,38 @@
 from tkinter import *
 from constants import *
+import random
+def draw_grid():
+    for row in range(height_screen):  # Loop through the rows
+        for col in range(width_screen):  # Loop through the columns
+                x1 = col * cell_size
+                y1 = row * cell_size
+                x2 = x1 + cell_size
+                y2 = y1 +cell_size
+                # If the cell is alive
+                if grid[row][col] == 1:
+                   canvas.create_rectangle(
+                        x1, y1, x2, y2, fill='blue', outline='black')
+                # If the cell is dead
+                else:
+                    canvas.create_rectangle(
+                        x1, y1, x2, y2, fill='white', outline='black')
+def create_grid():
+      grid = []
+      for row in range(height_screen):  # Loop through the rows
+            grid.append([])
+            for col in range(width_screen):  # Loop through the columns
+                # Append a random number to the grid
+                grid[row].append(random.randint(0, 1))
+      return grid
 root = Tk()
 root.title("Game of life")
 config_screen= "{0}x{1}".format(width_screen,height_screen)
 root.geometry(config_screen)
 root.minsize(min_width_screen,min_height_screen)
-mainFrame = Frame(root,width=width_screen,height=height_screen)
-mainFrame.grid()
+grid = create_grid()
+canvas = Canvas(root,width=width_screen*cell_size,height=height_screen*cell_size,bg="white")
+canvas.pack()
+draw_grid()
 
-root.mainloop() #отображение окна
+root.mainloop() 
+
